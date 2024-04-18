ce6844953148be2ae2fa0523812fd257b0b3fe52
lucasens1 <lucasensini1@gmail.com>
2024-04-18 15:33:06 +0200

    Images added to the HTML dinamically

--- js/main.js
+++ js/main.js
@@ -31,6 +31,6 @@ createApp({
                 text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
               },
             ],
-        },
+        };
       },
 }).mount('#app')
